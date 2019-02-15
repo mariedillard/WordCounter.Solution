05306f0b2aabe34bbb6f11f13c1d176c9f30d91f
Marie Dillard <mariedillard@protonmail.com>
2019-02-15 13:20:23 -0800

    adds model for future tests.

---
+++ wordSearch/Models/wordSearch.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace wordSearch
+{
+  public class RepeatCounter
+  {
+    public static bool testOne ()
+    {
+      string userInput = "Hey, how are you! This 1 is a dog, this 1 is a dog too. This1 is a dog! Dog?";
+      string inputAlphanumeric = String.Concat(userInput.Where(c => Char.IsWhiteSpace(c) || Char.IsLetterOrDigit(c)));
+      string inputLower = inputAlphanumeric.ToLower();
+      string[] trueInput = inputLower.Split();
+      int foundNumbers = 0;
+      string userWord = "dog";
+      for (int i = 0; i < trueInput.Length; i++)
+      {
+        if (trueInput[i].Contains(userWord))
+        {
+          foundNumbers ++;
+        }
+      }
+      if (foundNumbers == 4)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+    public static bool testTwo ()
+    {
+      string userInput = "Hey, how are you! This 1 is a dog, this 1 is a dog too. This1 is a dog! Dog?";
+      string inputAlphanumeric = String.Concat(userInput.Where(c => Char.IsWhiteSpace(c) || Char.IsLetterOrDigit(c)));
+      string inputLower = inputAlphanumeric.ToLower();
+      string[] trueInput = inputLower.Split();
+      int foundNumbers = 0;
+      string userWord = "1";
+      for (int i = 0; i < trueInput.Length; i++)
+      {
+        if (trueInput[i].Contains(userWord))
+        {
+          foundNumbers ++;
+        }
+      }
+      if (foundNumbers == 3)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+  }
+}
