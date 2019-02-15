832880a1f7ad0d572e2c29a83f8752c570e4d27a
Marie Dillard <mariedillard@protonmail.com>
2019-02-15 13:49:02 -0800

    adds working console program!

--- main.cs
+++ main.cs
@@ -1,12 +1,32 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class wordSearch
 {
-  public string userWriting { get; set; }
   public class Program
   {
     public static void Main() {
+      Console.WriteLine("Hi! I'm a program to help you find how many words you've used in a sentence!");
+      Console.WriteLine("Enter your sentence here:");
+      string userInput = Console.ReadLine();
+      Console.WriteLine("Enter the word you'd like to search for.");
+      string userWord = Console.ReadLine();
 
+      string inputAlphanumeric = String.Concat(userInput.Where(c => Char.IsWhiteSpace(c) || Char.IsLetterOrDigit(c)));
+      string inputLower = inputAlphanumeric.ToLower();
+      string[] trueInput = inputLower.Split();
+
+      int foundNumbers = 0;
+
+      for (int i = 0; i < trueInput.Length; i++)
+      {
+        if (trueInput[i].Contains(userWord))
+        {
+          foundNumbers ++;
+        }
+      }
+      Console.WriteLine("You used the word" + " *" + userWord + "* " + foundNumbers + " times.");
     }
   }
 }
